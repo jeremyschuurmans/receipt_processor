d84f04bba224080757b80f1e46b041506b5e0397
Jeremy Schuurmans <jeremyschuurmans@redsquirrel.local>
2023-02-02 15:09:24 -0600

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  namespace :receipts do
-    post :process
-    get :points
-  end
+  post "/receipts/process", to: "receipts#create"
+  get "/receipts/:id/points", to: "receipts#points"
 end
