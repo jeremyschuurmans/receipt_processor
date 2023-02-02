2390a5f1a6076235f1f71f0b0994c0460b6c3afa
Jeremy Schuurmans <jeremyschuurmans@redsquirrel.local>
2023-02-01 18:10:33 -0600

    create point calculator

---
+++ lib/point_calculator.rb
@@ -0,0 +1,60 @@
+class PointCalculator
+    def initialize(receipt)
+        @receipt = receipt
+        @points = 0
+    end
+
+    def process_receipt
+        process_retailer_name
+        process_round_dollar_amount_total
+        process_multiple_of_total
+        process_every_two_items
+        process_items_length
+        process_purchase_date
+        process_purchase_time
+
+        @points
+    end
+
+    def process_retailer_name
+        # One point for every alphanumeric character in the retailer name.
+        @points += @receipt.retailer.gsub(/[^0-9a-z]/i, '').length
+    end
+
+    def process_round_dollar_amount_total
+        # 50 points if the total is a round dollar amount with no cents
+        @receipt.total.split(".").last == "00" ? @points += 50 :  @points += 0
+    end
+
+    def process_multiple_of_total
+        # 25 points if the total is a multiple of 0.25
+        @receipt.total.to_f % 0.25 == 0 ? @points += 25 : @points += 0
+    end
+
+    def process_every_two_items
+        # 5 points for every two items on the receipt.
+        @points += 5 * (@receipt.items.length / 2)
+    end
+
+    def process_items_length
+        # If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
+        # The result is the number of points earned.
+        @receipt.items.each do |item|
+            if item[:shortDescription].strip.length % 3 == 0
+                @points += (item[:price].to_f * 0.2).ceil
+            end
+        end
+
+        @points
+    end
+
+    def process_purchase_date
+        # 6 points if the day in the purchase date is odd.
+        @receipt.purchase_date.split("-").last.to_i.odd? ? @points += 6 : @points += 0
+    end
+
+    def process_purchase_time
+        # 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+        @receipt.purchase_time.to_datetime > "14:00".to_datetime && @receipt.purchase_time.to_datetime < "16:00".to_datetime ? @points += 10 : @points += 0
+    end
+end
---
+++ spec/lib/point_calculator_spec.rb
@@ -0,0 +1,152 @@
+require "rails_helper"
+require "point_calculator"
+
+describe "PointCalculator" do
+  JSON_SAMPLE_ONE = {
+    "retailer": "Target",
+    "purchaseDate": "2022-01-01",
+    "purchaseTime": "13:01",
+    "items": [
+      {
+        "shortDescription": "Mountain Dew 12PK",
+        "price": "6.49"
+      },{
+        "shortDescription": "Emils Cheese Pizza",
+        "price": "12.25"
+      },{
+        "shortDescription": "Knorr Creamy Chicken",
+        "price": "1.26"
+      },{
+        "shortDescription": "Doritos Nacho Cheese",
+        "price": "3.35"
+      },{
+        "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+        "price": "12.00"
+      }
+    ],
+    "total": "35.35"
+  }
+
+  JSON_SAMPLE_TWO = {
+    "retailer": "M&M Corner Market",
+    "purchaseDate": "2022-03-20",
+    "purchaseTime": "14:33",
+    "items": [
+      {
+        "shortDescription": "Gatorade",
+        "price": "2.25"
+      },{
+        "shortDescription": "Gatorade",
+        "price": "2.25"
+      },{
+        "shortDescription": "Gatorade",
+        "price": "2.25"
+      },{
+        "shortDescription": "Gatorade",
+        "price": "2.25"
+      }
+    ],
+    "total": "9.00"
+  }
+
+  let(:receipt_one) { Receipt.new(
+    JSON_SAMPLE_ONE[:retailer],
+    JSON_SAMPLE_ONE[:purchaseDate],
+    JSON_SAMPLE_ONE[:purchaseTime],
+    JSON_SAMPLE_ONE[:items],
+    JSON_SAMPLE_ONE[:total],
+    )
+  }
+
+  let(:receipt_two) { Receipt.new(
+    JSON_SAMPLE_TWO[:retailer],
+    JSON_SAMPLE_TWO[:purchaseDate],
+    JSON_SAMPLE_TWO[:purchaseTime],
+    JSON_SAMPLE_TWO[:items],
+    JSON_SAMPLE_TWO[:total],
+    )
+  }
+
+  let(:point_calculator_first_receipt) { PointCalculator.new(receipt_one) }
+  let(:point_calculator_second_receipt) { PointCalculator.new(receipt_two) }
+
+  describe "#process_retailer_name" do
+    it "calculates one point for every alphanumeric character in the retailer name when there are no spaces or non-alphanumeric characters" do
+      expect(point_calculator_first_receipt.process_retailer_name).to eq(6)
+    end
+
+    it "calculates one point for every alphanumeric character in the retailer name when there are spaces and/or non-alphanumeric characters" do      
+      expect(point_calculator_second_receipt.process_retailer_name).to eq(14)
+    end
+  end
+
+  describe "#process_round_dollar_amount_total" do
+    it "adds fifty points if total amount is a round dollar amount with no cents" do
+      expect(point_calculator_second_receipt.process_round_dollar_amount_total).to eq(50)
+    end
+
+    it "does not add fifty points if total amount is not a round dollar amount with no cents" do
+      expect(point_calculator_first_receipt.process_round_dollar_amount_total).to eq(0)
+    end
+  end
+
+  describe "#process_multiple_of_total" do
+    it "adds twenty-five points if total is a multiple of 0.25" do
+      expect(point_calculator_second_receipt.process_multiple_of_total).to eq(25)
+    end
+
+    it "does not add twenty-five points if total is not a multiple of 0.25" do
+      expect(point_calculator_first_receipt.process_multiple_of_total).to eq(0)
+    end
+  end
+
+  describe "#process_every_two_items" do
+    it "adds five points for every two items on the receipt" do
+      expect(point_calculator_first_receipt.process_every_two_items).to eq(10)
+    end
+  end
+
+  describe "#process_items_length" do
+    it "multiplies the price of an item by 0.2 and rounds up if the trimmed length of the item description is a multiple of 3" do
+      expect(point_calculator_first_receipt.process_items_length).to eq(6)
+    end
+
+    it "does not multiply the price of an item by 0.2 and round up if the trimmed length of the item description is not a multiple of 3" do
+      expect(point_calculator_second_receipt.process_items_length).to eq(0)
+    end
+  end
+
+  describe "#process_purchase_date" do
+    it "adds six points if the day in the purchase date is odd" do
+      expect(point_calculator_first_receipt.process_purchase_date).to eq(6)
+    end
+
+    it "does not add six points if the day in the purchase date is even" do
+      expect(point_calculator_second_receipt.process_purchase_date).to eq(0)
+    end
+  end
+
+  describe "#process_purchase_time" do
+    it "adds ten points if the time of purchase is after 2:00pm and before 4:00pm" do
+      expect(point_calculator_second_receipt.process_purchase_time).to eq(10)
+    end
+
+    it "does not add ten points if the time of purchase is not after 2:00pm and before 4:00pm" do
+      expect(point_calculator_first_receipt.process_purchase_time).to eq(0)
+    end
+  end
+
+  describe "#process_receipt" do
+    it "calculates the correct number of points for a given receipt" do
+      processed_receipt = point_calculator_first_receipt.process_receipt
+
+      expect(processed_receipt).to eq(28)
+    end
+
+    it "calculates the correct number of points for a given receipt" do
+      processed_receipt = point_calculator_second_receipt.process_receipt
+
+      expect(processed_receipt).to eq(109)
+    end
+  end
+end
